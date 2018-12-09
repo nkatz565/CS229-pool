Fix action not clipped copy bug; revise action and mu scaling and clipping

src/model/a3c/a3c.py
@@ -6,9 +6,10 @@ import torch.nn.functional as F
 import torch.multiprocessing as mp
 
 from .net import Net
-from .worker import Worker, to_tensor, norm_state
+from .worker import Worker, norm_state
 from .shared_adam import SharedAdam
 from ..env import PoolEnv
+from .utils import v_wrap
 
 
 HIDDEN_DIM = 100
@@ -16,7 +17,7 @@ LR = 0.002
 
 def choose_action(state, model, action_space, w, h):
     s = norm_state(state, w, h)
-    return model.clip_action(model.choose_action(to_tensor(s)))
+    return model.clip_action(model.choose_action(v_wrap(s[None, :])))
 
 def save_model(filepath, model):
     torch.save(model.state_dict(), filepath)

src/model/a3c/net.py
@@ -1,18 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
-from .utils import set_init
+from .utils import set_init, norm, ratio
 
 
 class Net(nn.Module):
-    def __init__(self, s_dim, a_dim, h_dim, action_ranges=None, scale=2):
+    def __init__(self, s_dim, a_dim, h_dim, action_ranges=None):
         super().__init__()
 
         self.s_dim = s_dim
         self.a_dim = a_dim
         self.action_ranges = action_ranges
-        self.scale = scale
+        mu_scales = np.ones(len(action_ranges))
+        if action_ranges is not None:
+            for i in range(len(action_ranges)):
+                r = action_ranges[i]
+                scale = (r[1] - r[0]) / 2
+                mu_scales[i] = scale
+        self.mu_scales = torch.tensor(mu_scales, requires_grad=False).float()
 
         # Actor
         self.a1 = nn.Linear(s_dim, h_dim)
@@ -32,8 +39,8 @@ class Net(nn.Module):
     def forward(self, x):
         a1 = F.relu(self.a1(x))
         # TODO: not sure if using sigmoid to compress the range is a good idea, since 0 and 1 are unapproachable values
-        mu = (self.scale / 2) * torch.tanh(self.mu(a1))
-        sigma = F.softplus(self.sigma(a1)) + 0.001 # avoid 0
+        mu = torch.tanh(self.mu(a1)) * self.mu_scales
+        sigma = F.softplus(self.sigma(a1)) + 0.0001 # avoid 0
 
         c1 = F.relu(self.c1(x))
         values = self.v(c1)
@@ -44,15 +51,19 @@ class Net(nn.Module):
         self.train(False)
         mu, sigma, _ = self.forward(s)
         m = self.distribution(mu, sigma)
-        a = m.sample().numpy()
+        a = m.sample().view(-1).numpy()
 
         return a
 
-    def clip_action(self, a):
+    def clip_action(self, action):
+        a = np.copy(action)
         if self.action_ranges is not None:
             for i in range(a.size):
-                a[i] = (a[i] + self.scale / 2) / self.scale
-                a[i] = a[i].clip(*self.action_ranges[i])
+                r = self.action_ranges[i]
+                mn = norm(r[0], r[1], r[0])
+                mx = norm(r[1], r[1], r[0])
+                a[i] = a[i].clip(mn, mx) # [-0.5, 0.5]
+                a[i] = ratio(a[i], mx, mn) * (r[1] - r[0]) + r[0]
         return a
 
     def loss_func(self, s, a, v_t):

src/model/a3c/worker.py
@@ -2,24 +2,17 @@ import torch
 import torch.multiprocessing as mp
 import datetime
 
-from .utils import v_wrap, set_init, push_and_pull, record
+from .utils import v_wrap, set_init, push_and_pull, record, norm
 from .net import Net
 from ..env import PoolEnv
 
 
 GLOBAL_UPDATE_RATE = 5 # the network will sync with the global network every X iterations
 
-def to_tensor(s):
-    """Wraps a state representation into flat tensor"""
-    return torch.tensor(s).float().view(-1)
-
 def norm_state(s, w, h):
     s[::2] /= w
     s[1::2] /= h
     return s
-
-def norm(v, max_v, min_v):
-    return (v - (max_v + min_v) / 2) / (max_v - min_v)
 
 class Worker(mp.Process):
     def __init__(self, gnet, opt, global_ep, global_ep_r, name, env_params, HIDDEN_DIM, episodes, episode_length, model_path=None):
@@ -51,8 +44,7 @@ class Worker(mp.Process):
             rewards = 0 # accumulate rewards for each episode
             done = False
             for t in range(self.episode_length):
-                # Agent takes action using epsilon-greedy algorithm, get reward
-                action = self.lnet.choose_action(to_tensor(state))
+                action = self.lnet.choose_action(v_wrap(state[None, :]))
                 a = self.lnet.clip_action(action)
                 next_state, reward, done = env.step(a)
                 next_state = norm_state(next_state, env.state_space.w, env.state_space.h)